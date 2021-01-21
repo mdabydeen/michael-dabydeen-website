c2fe2be527418661b5cab60116c04b39dcbe0abe
Mike Dabydeen <mdabydeen@gmail.com>
2021-01-21 17:22:01 -0500

    Added netlify cms

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home({ slugs }) {
+
+export default function Home() {
+
+  useEffect(() => {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
 
       <main className={styles.main}>
